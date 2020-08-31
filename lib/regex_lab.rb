30411942dd8c581060657394c179b36dc382a215
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-31 23:23:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.scan(/^[A-Z].*[a-zA-Z][.]$/) 
+  if text.match(/^[A-Z]/) && text.match(/[\.]$/)
     return true
   else
     false
