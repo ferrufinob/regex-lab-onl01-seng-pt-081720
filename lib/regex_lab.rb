2f9774910af14074904a483186b3a57232ed645e
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-31 23:21:11 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,8 +17,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
-end
+  text.scan(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
+
 end
 
 def valid_phone_number?(phone)
